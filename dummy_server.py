6c559498f2ac5958c34f0100b3930cfd1a2c6702
msbizrise-arch <msbizrise@gmail.com>
2026-01-30 14:47:30 +0530

    Create dummy_server.py

---
+++ dummy_server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask("dummy_server")
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
